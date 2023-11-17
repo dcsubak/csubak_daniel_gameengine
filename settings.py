eb9eafab728cb3f6647f92c5c14f23b2bac948da
dcsubak <142929904+dcsubak@users.noreply.github.com>
2023-11-16 18:45:42 -0800

    Updated settings.py

--- settings.py
+++ settings.py
@@ -1,18 +1,23 @@
 # This file was created by: Daniel Csubak
 # Content from Chris Bradfield; Kids Can Code
-# Video link: https:///youtu.be/OmlQ0XCvIn0
- 
-# game settings 
-WIDTH = 360
-HEIGHT = 480
-FPS = 60
-SCORE = 0
+# KidsCanCode - Game Development with Pygame video series
+# Video link: https://youtu.be/OmlQ0XCvIn0 
 
-# player settings
-PLAYER_JUMP = 30
-PLAYER_GRAV = 1.5
+# game settings 
+WIDTH = 400
+HEIGHT = 600
+FPS = 60
+
+# player settings/constants
+PLATFORM_WIDTH = 100
+PLATFORM_HEIGHT = 20
+BOTTOM_PLATFORM_HEIGHT = 20
+PLAYER_JUMP = 20
+PLAYER_GRAV = 1.2
 global PLAYER_FRIC
-PLAYER_FRIC = 0.3
+PLAYER_FRIC = 0.2
+SCROLL_SPEED = 1
+MOB_SPEED = 3
 
 # define colors
 WHITE = (255, 255, 255)
@@ -20,11 +25,5 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-ORANGE = (255, 165, 0)
 
- 
-PLATFROM_LIST = [(0, HEIGHT - 40, WIDTH, 40, "ice"),
-                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20, "normal"),
-                 (125, HEIGHT - 350, 100, 20, "moving")
-                 (350,200, 100, 20, "normal"),
-                 (175, 100, 50, 20, "normal")]
+
